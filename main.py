4d562f91c364d7d9b496b001ee33c4b93dbc7b2f
Shivraj <shivunofficial003@gmail.com>
2022-04-14 02:51:18 +0530

    2nd

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import discord
 from discord.ext import commands
-from music import music
-
-cogs = [music]
-
-client = commands.Bot(command_prefix='?', intents= discord.Intents.all())
-
-for i in range(len(cogs)):
-    cogs[i].setup(client)
 
 
-client.run('OTYzNDQ0MzU4NTI1MzU4MTUw.YlWLew.GkGulTWZ25gvDkvjP85vEV6xsqg')
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='/')
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(help_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+bot.run('OTYzNDQ0MzU4NTI1MzU4MTUw.YlWLew.6sEfrW0kviirJNo8fmpHuNBv11I')
